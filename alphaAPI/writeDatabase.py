7c7772b3e250bad328de0af923097ff8473e274a
Jordan Snow <ssnowj@gmail.com>
2017-11-26 17:15:19 -0500

    Updated database writter to include volume and did some minor improvements for readablity

--- alphaAPI/writeDatabase.py
+++ alphaAPI/writeDatabase.py
@@ -2,50 +2,44 @@ import csv
 import json
 import MySQLdb
 
-TICKERS = ['GOOGL','MSFT','AAPL','AMZN']
-INDICATORS = ['3. low', '2. high','1. open', '4. close','SMA','EMA','ADX','RSI']
+TICKERS = ['GOOGL', 'MSFT', 'AAPL', 'AMZN']
+INDICATORS = ['3. low', '2. high', '1. open', '4. close', '5. volume', 'SMA', 'EMA', 'ADX', 'RSI']
 
 def converter():
-
-    conn = MySQLdb.connect(host='35.199.8.217', db='stocks', user='snow', passwd='stocklearnerpass')
-    print "Connected"
+    """Adds most of the data to the database, besides volume"""
+    conn = MySQLdb.connect(host='35.199.8.217', db='stocks', \
+                           user='snow', passwd='stocklearnerpasswd')
     curse = conn.cursor()
     curse.execute('USE stocks')
-    t = 2
+    ticks = 1
     for company in TICKERS:
-        with open('./'+company+'combo.json', 'r') as reader:
+        with open('./results/'+company+'combo.json', 'r') as reader:
             data = json.load(reader)
 
         data = data['Time Series (Daily)']
 
         for stock in data:
-            stockList = [stock]
-            c = 0
-            for indicators in INDICATORS:
-                if indicators == '5. volume':
-                    continue
+            stock_list = [stock]
+            count = 0
+            for _ in INDICATORS:
                 try:
-                    stockList.append(float(data[stock][INDICATORS[c]]))
+                    stock_list.append(float(data[stock][INDICATORS[count]]))
                 except:
-                    stockList.append(None)
+                    stock_list.append(None)
 
-                c += 1
+                count += 1
 
-	    #print c
-	    #print stockList
             curse.execute("INSERT INTO indexes \
-            (date, low, high, open, close, SMA, EMA, RSI, ADX, tickerID) \
-	    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",\
-	    (stockList[0],stockList[1],stockList[2],stockList[3], stockList[4]\
-	    ,stockList[5],stockList[6],stockList[7],stockList[8], t))
+            (date, low, high, open, close, volume, SMA, EMA, RSI, ADX, tickerID) \
+    	    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",\
+    	    (stock_list[0], stock_list[1], stock_list[2], stock_list[3], stock_list[4],  \
+    	    stock_list[5], stock_list[6], stock_list[7], stock_list[8], stock_list[9], ticks))
 
-            conn.commit()
-            #print stockList
+        conn.commit()
 
-        t+=1
+        ticks += 1
+
     conn.close()
-
-
 
 if __name__ == "__main__":
     converter()
